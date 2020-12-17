d2890c8bb90106dff98eb4149e9d111821e3ad99
Bryan Alves <bryanalves@gmail.com>
2020-12-16 23:05:38 -0500

    Problem 13: Part 2

--- src/aoc13.ts
+++ src/aoc13.ts
@@ -1,7 +1,5 @@
 import fs from 'fs';
 
-type part1position = [number, number, string];
-
 function parsedInput() {
   const input: string = fs.readFileSync('./res/aoc13.txt', 'utf8');
   // const input = `939
@@ -10,17 +8,16 @@ function parsedInput() {
   const parsed = input.split("\n").slice(0, -1);
   return [
     parseInt(parsed[0]),
-    parsed[1]
-      .split(',')
-      .filter((i) => { return i !== 'x' })
-      .map((i) => { return parseInt(i) })
+    parsed[1].split(',')
   ]
 }
 
 function part1() {
   const input = parsedInput();
   const start = input[0] as number;
-  const candidates = input[1] as number[];
+  const candidates = (input[1] as string[])
+      .filter((i) => { return i !== 'x' })
+      .map((i) => { return parseInt(i) })
 
   for (let i = start ; true ; i++) {
     let match = candidates.find((c) => { return i % c === 0});
@@ -30,4 +27,45 @@ function part1() {
   }
 }
 
+const absoluteModulo = (a:number, b:number) => ((a % b) + b) % b
+
+function chineseRemainderTheorem(busses: any[]) {
+  const n = busses.slice(1).reduce((acc, cur) => {
+    if (cur === 'x') return acc;
+    return acc * cur
+  }, busses[0]);
+
+  const sum = busses.reduce((acc, cur, idx) => {
+    if (cur === 'x') return acc;
+    const abs = absoluteModulo(cur - idx, cur)
+    const nu = n / cur
+    const inverse = modularInverse(nu, cur);
+
+    return acc + BigInt(BigInt(abs) * BigInt(nu) * BigInt(inverse));
+  }, BigInt(0));
+
+  return sum % BigInt(n);
+}
+
+function modularInverse(a:number, mod:number) {
+  const b = a % mod;
+
+  for (let i = 1; i < mod ; i++) {
+    if (((b * i) % mod) === 1) {
+      return i
+    }
+  }
+
+  return 1;
+}
+
+function part2() {
+  const input = parsedInput();
+  const busses = (input[1] as string[])
+      .map((bus) => (bus === 'x') ? 'x' : parseInt(bus, 10))
+
+  return chineseRemainderTheorem(busses);
+}
+
 console.log(part1());
+console.log(part2());

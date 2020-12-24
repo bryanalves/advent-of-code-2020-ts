de7dfe1837a57e02adf334a04130a2a1b0f9bbf9
Bryan Alves <bryanalves@gmail.com>
2020-12-24 15:57:37 -0500

    Problem 24: Part 2

--- src/aoc24.ts
+++ src/aoc24.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-function parsedInput() {
+function parsedInput(): string[][] {
   const input: string = fs.readFileSync('./res/aoc24.txt', 'utf8');
 
   // const input = `sesenwnenenewseeswwswswwnenewsewsw
@@ -69,10 +69,14 @@ function move(from: number[], direction: string) {
   return [0, 0, 0]
 }
 
-function part1() {
+function get_neighbors(from: number[]) {
+  return ["e", "w", "se", "sw", "ne", "nw"].map((direction) => move(from, direction))
+}
+
+function init_tiles(input: string[][]) {
   const flipped = new Set<string>()
 
-  parsedInput().forEach((dirset) => {
+  input.forEach((dirset) => {
     const tile = dirset.reduce((acc, dir) => {
       acc = move(acc, dir!)
       return acc
@@ -85,7 +89,67 @@ function part1() {
     }
   })
 
+  return flipped
+}
+
+function part1() {
+  const flipped = init_tiles(parsedInput()!)
+
   return flipped.size
 }
 
+function iterate(blacktiles: Set<string>): Set<string> {
+  const retval = new Set<string>()
+
+  const blacktilesarr = set_to_numarray(blacktiles)
+
+  const whitetiles =
+    new Set<string>(
+      blacktilesarr
+        .reduce((acc, tile) => acc.concat(get_neighbors(tile)), [])
+        .filter((tile) => !blacktiles.has(tile.join(',')))
+        .map((tile) => tile.join(','))
+    )
+
+  const whitetilesnum = set_to_numarray(whitetiles)
+
+  blacktilesarr.forEach((tile) => {
+    const flipped = flipped_neighbors(tile, blacktiles)
+    if ((flipped.length == 1) || (flipped.length == 2))
+      retval.add(tile.join(','))
+  })
+
+  whitetilesnum.forEach((tile) => {
+    const flipped = flipped_neighbors(tile, blacktiles)
+    if (flipped.length == 2)
+      retval.add(tile.join(','))
+  })
+
+  return retval
+}
+
+function flipped_neighbors(tile: number[], blacktiles: Set<string>) {
+  return get_neighbors(tile).filter((neighbor) => blacktiles.has(neighbor.join(',')))
+}
+
+function set_to_numarray(input: Set<string>): number[][] {
+  return Object.assign([], Array.from(input)
+    .map((tile) =>
+      tile.split(',')
+      .map((coord) => parseInt(coord, 10))
+    )
+  )
+}
+
+function part2() {
+  let flipped = init_tiles(parsedInput()!)
+  for (let i = 0 ; i < 100 ; i++) {
+    flipped = iterate(flipped)
+  }
+
+  return flipped.size
+
+}
+
 console.log(part1())
+console.log(part2())
